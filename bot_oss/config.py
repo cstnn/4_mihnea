9efbfee82c9d5ff34078a0def13e644031b96f1d
cstnn <35491404+cstnn@users.noreply.github.com>
2019-11-23 16:17:27 +0200

    bot

--- bot_oss/config.py
+++ bot_oss/config.py
@@ -6,7 +6,7 @@
 
 keys = {
     "sandy_url" : "https://oss.hpcloud.hp.com/os/SalesAdminView?mode=adv&default=1",
-    "my_user" : "costin.nadolu@hp.com",
+    "my_user" : "company_email_here",
     "my_pass" : "password_here",
     #"hp_on" : "111",
     "hp_on" : input("Please provide the HP# :"),
